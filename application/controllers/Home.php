ea34330a5f8c0833efd48551cc236cec8041f619
Alberto <oalberto@gmail.com>
2021-09-22 23:00:45 -0300

    fatiando o tema

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,11 +6,14 @@ class Home extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        // $this->load->helper('url');
     }
 
     public function index()
     {
+        $this->load->view('layout/header');
         $this->load->view('home/index');
+        $this->load->view('layout/footer');
     }
 
 }
